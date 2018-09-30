d16763d7625e8a6fab16b75ec8befcbb1dda0d83
fatah banan wijaya <fatah.banan@gmail.com>
2018-09-30 17:31:23 +0700

    tugasweek2-function

--- ANCHOR-EXCERCISE3-IfElseRevisi.js
+++ ANCHOR-EXCERCISE3-IfElseRevisi.js
@@ -1,24 +1,22 @@
-
-
-var nama= 'zero';
-var peran= 'penyihir';
+var nama = 'tono '
+var peran= 'penyihir '
 
 if (nama === '' && peran === ''){
-console.log('Nama harus diisi!');
+  console.log('nama harus diisi')
 }
-  else if (nama ''){
-  console.log('halo'+ nama + 'piih peranmu untuk memulai game!');
-} 
-  else if (peran === 'ksatria'){
-  console.log('Selamat datang di Dunia Proxytia, ' + nama);
-  console.log('halo ' + peran +' '+ nama + ' ,kamu dapat menyerang dengan senjatamu!');
-}
-
-  else if (peran === 'tabib'){
-  console.log('Selamat datang di Dunia Proxytia, ' + nama);
-  console.log('halo ' + peran +' '+ nama +' ,kamu akan membantu temanmu yang terluka.');
-}
-  else if (peran === 'penyihir'){
-  console.log('Selamat datang di Dunia Proxytia, ' + nama);
-  console.log('halo ' + peran +' '+ nama + ' ,ciptakan keajaiban yang membantu kemenanganmu!');
-}
+  else if(peran==''){
+  console.log('halo ' + nama + 'pilih peranmu untuk memulai game!');
+  }
+  else if(peran==='ksatria '){
+    console.log(' selamat datang di dunia proxytia ' + nama);
+    console.log('halo '+ peran +''+ nama+ ' ,kau dapat menyerang!');
+  }
+  else if(peran ==='dokter '){
+    console.log('selamat datang di dunia proxytia ' + nama);
+    console.log('halo ' + peran + ''+ nama + ' obati teman temanmu');
+  }
+  else if (peran === 'penyihir '){
+    console.log('selamat datang di dunia proxytia '+ nama);
+    
+    console.log('halo '+ peran +''+ nama + ' ciptakan keajaiban' );
+  }
